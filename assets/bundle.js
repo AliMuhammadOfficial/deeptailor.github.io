6e008050db8defe3f6a96a41c3c30d898e5e900e
Deep Tailor <deeptailor@gmail.com>
2016-11-26 12:53:34 -0800

    menu bar add

--- assets/bundle.js
+++ assets/bundle.js
@@ -50,6 +50,11 @@
 	document.addEventListener('DOMContentLoaded', () => {
 	  $('#nav-icon1').click(function(){
 			$(this).toggleClass('open');
+	    if($('#menu').hasClass('hidden')){
+	      $('#menu').removeClass('hidden').addClass('show')
+	    } else{
+	      $('#menu').removeClass('show').addClass('hidden')
+	    }
 		});
 	
 	});
--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,11 @@ const $ = require('jquery');
 document.addEventListener('DOMContentLoaded', () => {
   $('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
+    if($('#menu').hasClass('hidden')){
+      $('#menu').removeClass('hidden').addClass('show')
+    } else{
+      $('#menu').removeClass('show').addClass('hidden')
+    }
 	});
 
 });
